480120dfb6578b35da52c06415df2b18a76dc3d3
Jonathan Fraimorice <yonifra@gmail.com>
2016-05-24 17:51:44 +0300

    Added category class

---
+++ Versus.Common/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Versus.Common.Entities
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BackdropUrl { get; set; }
+        public string SmallIconUrl { get; set; }
+    }
+}
