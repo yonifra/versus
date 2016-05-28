fbb81b5469da77648164e73e3eddb703b8a30b99
yonifra <yonifra@gmail.com>
2016-05-29 00:20:52 +0300

    (re)added android files

---
+++ Droid/Adapters/CompetitionListAdapter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Square.Picasso;
+
+namespace Versus.Droid
+{
+    internal class CompetitionAdapterWrapper : Java.Lang.Object
+    {
+        public TextView Name { get; set; }
+        public TextView Description { get; set; }
+        public ImageView Backdrop { get; set; }
+    }
+
+    class CompetitionListAdapter : BaseAdapter
+    {
+        private readonly Activity context;
+        private readonly IEnumerable<VsCompetition> _competitions;
+
+        public CompetitionListAdapter (Activity context, IEnumerable<VsCompetition> competitions)
+        {
+            this.context = context;
+            this._competitions = competitions;
+        }
+
+        public override View GetView (int position, View convertView, ViewGroup parent)
+        {
+            if (position < 0)
+                return null;
+
+            var view = (convertView
+                       ?? context.LayoutInflater.Inflate (
+                    Resource.Layout.CompetitionListItemLayout, parent, false)
+                       );
+
+            if (view == null)
+                return null;
+
+            var wrapper = view.Tag as CompetitionAdapterWrapper;
+            if (wrapper == null) {
+                wrapper = new CompetitionAdapterWrapper {
+                    Title = view.FindViewById<TextView> (Resource.Id.item_title),
+                    Art = view.FindViewById<ImageView> (Resource.Id.item_image)
+                };
+                view.Tag = wrapper;
+            }
+
+            var movie = _competitions.ElementAt (position);
+
+            wrapper.Title.SetBackgroundResource (Android.Resource.Color.Transparent);
+            wrapper.Title.Text = movie.Title;
+
+            // Load the image asynchonously
+            Picasso.With (context).Load (movie.PosterUrl).Into (wrapper.Art);
+
+            return view;
+        }
+
+        public override int Count {
+            get { return _competitions.Count (); }
+        }
+
+        public override Java.Lang.Object GetItem (int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId (int position)
+        {
+            return position;
+        }
+
+        public override bool HasStableIds {
+            get {
+                return true;
+            }
+        }
+    }
+}
+
--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -15,12 +15,6 @@ namespace Versus.Droid
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button> (Resource.Id.myButton);
-
-            button.Click += delegate { button.Text = string.Format ("{0} clicks!", count++); };
         }
     }
 }
