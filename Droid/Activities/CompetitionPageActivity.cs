fcc7fd682674bd35f736e8066d65bb0e03379ea0
yonifra <yonifra@gmail.com>
2016-06-11 23:49:20 +0300

    Added competition page, with some basic info

--- Droid/Activities/CompetitionPageActivity.cs
+++ Droid/Activities/CompetitionPageActivity.cs
@@ -1,15 +1,9 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using Square.Picasso;
 using Versus.Portable.Data;
 
 namespace Versus.Droid
@@ -17,15 +11,29 @@ namespace Versus.Droid
     [Activity (Label = "Competition Page")]
     public class CompetitionPageActivity : Activity
     {
-        protected override void OnCreate (Bundle savedInstanceState)
+        protected async override void OnCreate (Bundle savedInstanceState)
         {
             base.OnCreate (savedInstanceState);
 
+            SetContentView (Resource.Layout.CompetitionPage);
+
             var competitionName = Intent.GetStringExtra ("competitionName") ?? "";
-            var competition = FirebaseManager.Instance.GetCompetition (competitionName);
+            var competition = await FirebaseManager.Instance.GetCompetition (competitionName);
 
             if (competition != null) {
                 //TODO: Do something with the competition
+
+                var e1Image = (ImageView)FindViewById (Resource.Id.entity1ImageView);
+                var e1Name = (TextView)FindViewById (Resource.Id.entity1Name);
+                var e2Image = (ImageView)FindViewById (Resource.Id.entity2ImageView);
+                var e2Name = (TextView)FindViewById (Resource.Id.entity2Name);
+
+
+                // Load the image asynchonously
+                Picasso.With (this).Load (competition.BackdropUrl).Into (e1Image);
+                Picasso.With (this).Load (competition.BackdropUrl).Into (e2Image);
+                e1Name.Text = "Entity 1";
+                e2Name.Text = "Entity 2";
             }
         }
     }
