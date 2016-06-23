3675b71f894af8a493e3e306e704395a1a405fa6
yonifra <yonifra@gmail.com>
2016-06-24 00:14:39 +0300

    Fixed all issues related to wrong packages. Now everything builds and runs

--- Droid/Activities/CompetitionsActivity.cs
+++ Droid/Activities/CompetitionsActivity.cs
@@ -1,78 +1,72 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Widget;
+using Versus.Droid.Activities;
 using Versus.Droid.Adapters;
 using Versus.Portable.Data;
 using Versus.Portable.Entities;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
 
-namespace Versus.Droid.Activities
+namespace Versus.Droid
 {
-    [Activity(Label = "Active Competitions")]
+    [Activity (Label = "Active Competitions")]
     public class CompetitionsActivity : BaseActivity
     {
-        protected override int LayoutResource => Resource.Layout.fragment_competitions;
+        protected override int LayoutResource {
+            get {
+                return Resource.Layout.fragment_competitions;
+            }
+        }
 
-        protected override async void OnCreate(Bundle savedInstanceState)
+        protected async override void OnCreate (Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate (savedInstanceState);
 
-            var categoryName = Intent.GetStringExtra("categoryName") ?? "";
+            var categoryName = Intent.GetStringExtra ("categoryName") ?? "";
             IEnumerable<VsCompetition> competitions;
 
-            if (categoryName == "")
-            {
-                Toast.MakeText(this, "Could not resolve category name", ToastLength.Short).Show();
-                var all = await FirebaseManager.Instance.GetAllCompetitions();
+            if (categoryName == "") {
+                Console.WriteLine("Could not resolve category name");
+                var all = await FirebaseManager.Instance.GetAllCompetitions ();
                 competitions = all.Values;
-            }
-            else
-            {
-                Toast.MakeText(this, "Found category " + categoryName, ToastLength.Short).Show();
-                competitions = await FirebaseManager.Instance.GetCompetitions(categoryName);
+            } else {
+                Toast.MakeText (this, "Found category " + categoryName, ToastLength.Short).Show ();
+                competitions = await FirebaseManager.Instance.GetCompetitions (categoryName);
             }
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.fragment_competitions);
+            SetContentView (Resource.Layout.fragment_competitions);
 
-            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            var toolbar = FindViewById<Toolbar> (Resource.Id.toolbar);
 
             //  ActionBar.Title = competitionName;
-            SetSupportActionBar(toolbar);
+            SetSupportActionBar (toolbar);
             SupportActionBar.Title = categoryName + " competitions";
 
-            var competitionsListView = FindViewById<ListView>(Resource.Id.competitionsListView);
+            var competitionsListView = FindViewById<ListView> (Resource.Id.competitionsListView);
 
-            competitionsListView.Adapter = new CompetitionListAdapter(this, competitions);
+            competitionsListView.Adapter = new CompetitionListAdapter (this, competitions);
 
-            competitionsListView.ItemClick += (sender, e) =>
-            {
+            competitionsListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
                 var index = e.Position;
 
-                var lv = sender as ListView;
+                var lv = (sender as ListView);
 
-                if (lv != null)
-                {
-                    var competitionListAdapter = lv.Adapter as CompetitionListAdapter;
+                if (lv != null) {
+                    var competition = (lv.Adapter as CompetitionListAdapter).Competitions [index];
+                    //  Toast.MakeText (this, competition.Name + " clicked", ToastLength.Short).Show ();
 
-                    if (competitionListAdapter != null)
-                    {
-                        var competition = competitionListAdapter.Competitions[index];
-                        //  Toast.MakeText (this, competition.Name + " clicked", ToastLength.Short).Show ();
+                    // Put the name of the selected category into the intent
+                    var competitionActivity = new Intent (this, typeof (CompetitionPageActivity));
+                    competitionActivity.PutExtra ("competitionName", competition.Name);
 
-                        // Put the name of the selected category into the intent
-                        var competitionActivity = new Intent(this, typeof(CompetitionPageActivity));
-                        competitionActivity.PutExtra("competitionName", competition.Name);
-
-                        // Start the competitions activity
-                        StartActivity(competitionActivity);
-                    }
-                }
-                else
-                {
-                    Toast.MakeText(this, "Item " + e.Position + " clicked", ToastLength.Short).Show();
+                    // Start the competitions activity
+                    StartActivity (competitionActivity);
+                } else {
+                    Toast.MakeText (this, "Item " + e.Position + " clicked", ToastLength.Short).Show ();
                 }
             };
         }
--- Droid/Activities/MainActivity.cs
+++ Droid/Activities/MainActivity.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
 using Versus.Droid.Fragments;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
 
@@ -8,6 +10,9 @@ namespace Versus.Droid.Activities
     [Activity (Label = "Versus", LaunchMode = Android.Content.PM.LaunchMode.SingleTop, MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : BaseActivity
     {
+      //  DrawerLayout drawerLayout;
+        NavigationView navigationView;
+
         protected override int LayoutResource => Resource.Layout.Main;
 
         protected override void OnCreate (Bundle bundle)
@@ -15,13 +20,30 @@ namespace Versus.Droid.Activities
             base.OnCreate (bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            //     SetContentView (Resource.Layout.Main);
 
             var toolbar = FindViewById<Toolbar> (Resource.Id.toolbar);
 
             //  ActionBar.Title = competitionName;
             SetSupportActionBar (toolbar);
             SupportActionBar.Title = "Versus";
+
+            navigationView = FindViewById<NavigationView> (Resource.Id.nav_view);
+            navigationView.NavigationItemSelected += (sender, e) => {
+                e.MenuItem.SetChecked (true);
+
+                switch (e.MenuItem.ItemId) {
+                case Resource.Id.nav_home:
+                    ListItemClicked (0);
+                    break;
+                case Resource.Id.nav_search:
+                    ListItemClicked (1);
+                    break;
+                case Resource.Id.nav_profile:
+                    ListItemClicked (2);
+                    break;
+                }
+            };
         }
 
         private void ListItemClicked (int position)
@@ -32,10 +54,8 @@ namespace Versus.Droid.Activities
                 fragment = new CategoriesFragment ();
                 break;
             case 1:
-          //      fragment = new ByCategoryFragment ();
                 break;
             case 2:
-     //           fragment = new SearchFragment ();
                 break;
             }
 
